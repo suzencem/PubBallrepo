98e333d866944e0cfa5390dafd24a42758b90d71
suzencem <suzen.cem@gmail.com>
2013-07-22 14:22:40 +0300

    Revert "imgelab sync commit"
    This reverts commit c2e3179357b88685fd3cd45e1d19f0663c2c2e59.

--- PubBall/src/com/example/pubball/Player.java
+++ PubBall/src/com/example/pubball/Player.java
@@ -8,21 +8,5 @@ package com.example.pubball;
 public class Player {
 	
 	//Data Field
-	private float pointX;
-	private float pointY;
 	
-	
-
-	public float getPointX() {
-		return pointX;
-	}
-	public void setPointX(float pointX) {
-		this.pointX = pointX;
-	}
-	public float getPointY() {
-		return pointY;
-	}
-	public void setPointY(float pointY) {
-		this.pointY = pointY;
-	}
 }
